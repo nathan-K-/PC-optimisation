5e6885b45e67a0d7bc3667117a1518c08acbc7f6
Kienlen Nathan <nkienlen@if501-208-09.insa-lyon.fr>
2017-11-27 11:29:50 +0100

    adding timers, lot of timers

--- src/Common.h
+++ src/Common.h
@@ -56,7 +56,7 @@ class Common {
     constexpr static double GRADIENT_SCALE = 30;
 
     const static int Number_Degradation_Step = 10;
-    const static int Number_Evolution_Step = 1000000;
+    const static int Number_Evolution_Step = 500;
 };
 
 
