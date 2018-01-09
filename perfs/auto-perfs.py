e8cfad86bd465e05b161c5b23e26c27de674e62e
nathan-k- <nathan.kienlen@gmail.com>
2018-01-09 08:49:36 +0100

    perf monitoring script (yay python)

--- perfs/auto-perfs.py
+++ perfs/auto-perfs.py
@@ -1,30 +1,43 @@
 import subprocess
+import csv
+import time
 
-nbr_exec = 5
+nbr_exec = 2
+nbr_threads = 4
 
-moyenne_total_time_ms = 0
+log_file = "perfs_log-" + str(int(time.time())) + ".csv"
 
-num_threads = 2
-for i in range(nbr_exec):
-    # ls = subprocess.run(["./run.sh", num_threads], stdout=subprocess.PIPE)
-    # output = ls.stdout.decode("UTF-8")
-    # print(output)
-    output = '-- Configuring done\n-- Generating done\n-- Build files have been written to: ' \
-             '/home/nathan/DEV/PC-optimisation\n[100%] Built target pdc_evol_model\nInit binding matrix\nDone in 440 ' \
-             'ms\nCreate World\nDone in 165 ms\nInitialize environment\nDone in 5 ms\nInitialize random ' \
-             'population\nSearching for a viable organism .Found !\nFilling the grid\nDone in 8630 ms\nRun ' \
-             'evolution\nEvolution at step 0 -- Number of Organism 1024  (Dead: 9 -- Mutant: 9)-- Min Fitness: 0.006481 -- ' \
-             'Max Fitness: 0.006481\nDone in 45958 ms\nTotal time : 55201 ms\nNon-regression test successful !\n '
+with open(log_file, 'a') as csvfile:
+    fieldnames = ["# of threads", "execution", "tests success", "total time (ms)"]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    output_splitted = output.split('\n')
+    writer.writeheader()
 
-    assert output_splitted[-2] == 'Non-regression test successful !'
+    for thr in range(1, nbr_threads+1):
 
-    actual_total_time_ms = output_splitted[-3].split(' ')[-2]
+        for i in range(nbr_exec):
+            ls = subprocess.run(["./run.sh", str(thr)], stdout=subprocess.PIPE)
+            output = ls.stdout.decode("UTF-8")
+            # print(output)
+            #output = '-- Configuring done\n-- Generating done\n-- Build files have been written to: ' \
+             ##        '/home/nathan/DEV/PC-optimisation\n[100%] Built target pdc_evol_model\nInit binding matrix\nDone in 440 ' \
+              #       'ms\nCreate World\nDone in 165 ms\nInitialize environment\nDone in 5 ms\nInitialize random ' \
+              #       'population\nSearching for a viable organism .Found !\nFilling the grid\nDone in 8630 ms\nRun ' \
+              #       'evolution\nEvolution at step 0 -- Number of Organism 1024  (Dead: 9 -- Mutant: 9)-- Min Fitness: 0.006481 -- ' \
+              #       'Max Fitness: 0.006481\nDone in 45958 ms\nTotal time : 55201 ms\nNon-regression test successful !\n '
 
-    assert actual_total_time_ms.isdigit()
-    moyenne_total_time_ms += int(actual_total_time_ms)
+            output_splitted = output.split('\n')
 
+            if output_splitted[-2] != 'Non-regression test successful !':
+                writer.writerow({"# of threads": thr, "execution": i, "tests success": False,
+                                 "total time (ms)": None})
+                csvfile.flush()
+                continue
 
-total_time_ms = moyenne_total_time_ms / nbr_exec
-print(total_time_ms)
+            actual_total_time_ms = output_splitted[-3].split(' ')[-2]
+
+            if actual_total_time_ms.isdigit():
+                writer.writerow({"# of threads": thr, "execution": i, "tests success": True,
+                                 "total time (ms)": actual_total_time_ms})
+                csvfile.flush()
+
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cmake .
-echo $1
 make
+echo $1
 export OMP_NUM_THREADS=$1
 ./pdc_evol_model
 cd test && python3 non_reg_ref.py
